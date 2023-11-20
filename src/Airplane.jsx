41a9b84f9d72472858a645e4f49e9a20e130fbb5
notharshprajapati <prajapati.harsh0243@gmail.com>
2023-11-21 02:06:37 +0530

    Airplane useFrame and position

--- src/Airplane.jsx
+++ src/Airplane.jsx
@@ -1,10 +1,28 @@
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Matrix4, Vector3 } from "three";
+
+export const planePosition = new Vector3(0, 3, 7);
 
 export function Airplane(props) {
   const { nodes, materials } = useGLTF("assets/models/airplane.glb");
   const groupRef = useRef();
   const helixMeshRef = useRef();
+
+  useFrame(({ camera }) => {
+    const matrix = new Matrix4().multiply(
+      new Matrix4().makeTranslation(
+        planePosition.x,
+        planePosition.y,
+        planePosition.z
+      )
+    );
+
+    groupRef.current.matrixAutoUpdate = false;
+    groupRef.current.matrix.copy(matrix);
+    groupRef.current.matrixWorldNeedsUpdate = true;
+  });
 
   return (
     <group ref={groupRef}>
