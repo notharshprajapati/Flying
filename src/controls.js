a70853cf5ec382a9851c00fe9450261c037419f1
notharshprajapati <prajapati.harsh0243@gmail.com>
2023-11-21 03:43:31 +0530

    boost added

--- src/controls.js
+++ src/controls.js
@@ -1,5 +1,9 @@
 export let controls = {};
 
+function easeOutQuad(x) {
+  return 1 - (1 - x) * (1 - x);
+}
+
 window.addEventListener("keydown", (e) => {
   controls[e.key.toLowerCase()] = true;
 });
@@ -11,11 +15,13 @@ let maxVelocity = 0.01;
 let jawVelocity = 0;
 let pitchVelocity = 0;
 let planeSpeed = 0.006;
+export let turbo = 0;
 
 export function updatePlaneAxis(x, y, z, planePosition, camera) {
   //ease in speed
   jawVelocity *= 0.95;
-  pitchVelocity *= 0.5;
+  if (controls.shift) pitchVelocity *= 0.95;
+  else pitchVelocity *= 0.5;
 
   if (Math.abs(jawVelocity) > maxVelocity)
     jawVelocity = Math.sign(jawVelocity) * maxVelocity;
@@ -23,20 +29,22 @@ export function updatePlaneAxis(x, y, z, planePosition, camera) {
   if (Math.abs(pitchVelocity) > maxVelocity)
     pitchVelocity = Math.sign(pitchVelocity) * maxVelocity;
 
-  if (controls["a"]) {
-    jawVelocity += 0.0025;
-  }
+  if (controls["a"]) jawVelocity += 0.0025;
 
-  if (controls["d"]) {
-    jawVelocity -= 0.0025;
-  }
+  if (controls["d"]) jawVelocity -= 0.0025;
 
-  if (controls["w"]) {
-    pitchVelocity -= 0.0025;
-  }
+  if (controls["w"]) pitchVelocity -= 0.0025;
 
-  if (controls["s"]) {
-    pitchVelocity += 0.0025;
+  if (controls["s"]) pitchVelocity += 0.0025;
+
+  if (controls["r"]) {
+    jawVelocity = 0;
+    pitchVelocity = 0;
+    turbo = 0;
+    x.set(1, 0, 0);
+    y.set(0, 1, 0);
+    z.set(0, 0, 1);
+    planePosition.set(0, 3, 7);
   }
 
   x.applyAxisAngle(z, jawVelocity);
@@ -49,5 +57,15 @@ export function updatePlaneAxis(x, y, z, planePosition, camera) {
   y.normalize();
   z.normalize();
 
-  planePosition.add(z.clone().multiplyScalar(-planeSpeed));
+  //plane posn & velocity
+  if (controls.shift) turbo += 0.025;
+  else turbo *= 0.95;
+
+  turbo = Math.min(Math.max(turbo, 0), 1);
+  let turboSpeed = easeOutQuad(turbo) * 0.02;
+
+  camera.fov = 45 + turboSpeed * 900;
+  camera.updateProjectionMatrix();
+
+  planePosition.add(z.clone().multiplyScalar(-planeSpeed - turboSpeed));
 }
