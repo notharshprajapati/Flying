b49549c07a94cd6630465820349e9125cbf11467
notharshprajapati <prajapati.harsh0243@gmail.com>
2023-11-21 04:14:32 +0530

    motion blur added

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,9 @@ import { SphereEnv } from "./SphereEnv";
 import { Landscape } from "./Landscape";
 import { Airplane } from "./Airplane";
 import { Targets } from "./Targets";
+import { EffectComposer, HueSaturation } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import { MotionBlur } from "./MotionBlur";
 
 function App() {
   return (
@@ -16,7 +19,6 @@ function App() {
       <Environment background={false} files={"assets/textures/envmap.hdr"} />
       <PerspectiveCamera makeDefault position={[0, 10, 10]} />
       {/* <OrbitControls target={[0, 0, 0]} /> */}
-
       <Landscape />
       <Airplane />
       <Targets />
@@ -35,6 +37,15 @@ function App() {
         shadow-camera-left={-6.2}
         shadow-camera-right={6.4}
       />
+      {/*  effects */}
+      <EffectComposer>
+        <MotionBlur />
+        <HueSaturation
+          blendFunction={BlendFunction.NORMAL} // blend mode
+          hue={-0.15} // hue in radians
+          saturation={0.1} // saturation in radians
+        />
+      </EffectComposer>
     </>
   );
 }
